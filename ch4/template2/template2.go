13645a1f22185bd95f106cf5ed19100ca044865e
jiehong.jh <jiehong.jh@alibaba-inc.com>
2023-02-14 15:09:33 +0800

    包的别名

--- ch4/template2/template2.go
+++ ch4/template2/template2.go
@@ -1,8 +1,14 @@
-package template2
+package main
 
 import (
-	"text/template"
+	"fmt"
+	ht "html/template"
+	"log"
+	"os"
+	tt "text/template"
 	"time"
+
+	"lemonguge.cn/learn_go/ch4/github"
 )
 
 // 一个模板是一个字符串或一个文件，里面包含了一个或多个由双花括号包含的 {{action}} 对象。
@@ -23,10 +29,46 @@ Age:    {{.CreatedAt | daysAgo}} days
 
 // template.New 函数先创建并返回一个模板；Funcs 方法将自定义函数注册到模板中，并返回模板；最后调用 Parse 函数分析模板。
 // template.Must 函数可以检测 error 是否为 nil（如果不是 nil 则发出 panic 异常），然后返回传入的模板。
-var report = template.Must(template.New("issuelist").
-	Funcs(template.FuncMap{"daysAgo": daysAgo}).
+var report = tt.Must(tt.New("issuelist").
+	Funcs(tt.FuncMap{"daysAgo": daysAgo}).
 	Parse(templ))
+
+// html/template 模板包使用和 text/template 包相同的 API 和模板语言，但是增加了一个将字符串自动转义特性。
+// 可以避免输入字符串和 HTML、JavaScript、CSS 或 URL 语法产生冲突的问题。以及一些长期存在的安全问题，比如 HTML 注入攻击。
+var report2 = ht.Must(ht.New("issuelist").Parse(`
+<h1>{{.TotalCount}} issues</h1>
+<table>
+<tr style='text-align: left'>
+  <th>#</th>
+  <th>State</th>
+  <th>User</th>
+  <th>Title</th>
+</tr>
+{{range .Items}}
+<tr>
+  <td><a href='{{.HTMLURL}}'>{{.Number}}</a></td>
+  <td>{{.State}}</td>
+  <td><a href='{{.User.HTMLURL}}'>{{.User.Login}}</a></td>
+  <td><a href='{{.HTMLURL}}'>{{.Title}}</a></td>
+</tr>
+{{end}}
+</table>
+`))
 
 func daysAgo(t time.Time) int {
 	return int(time.Since(t).Hours() / 24)
+}
+
+func main() {
+	result, err := github.SearchIssues([]string{"repo:golang/go", "is:open", "json", "decoder"})
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := report.Execute(os.Stdout, result); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("<===== =====>")
+	if err := report2.Execute(os.Stdout, result); err != nil {
+		log.Fatal(err)
+	}
 }
