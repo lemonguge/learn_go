dc2f3496b1416027383f408816c37c35f56076c7
jiehong.jh <jiehong.jh@alibaba-inc.com>
2023-02-20 10:44:29 +0800

    基于指针对象的方法

--- ch6/geometry/geometry.go
+++ ch6/geometry/geometry.go
@@ -5,6 +5,11 @@ import (
 	"math"
 )
 
+// 在每一个合法的方法调用表达式中，也就是下面三种情况里的任意一种情况都是可以的：
+// 1、接收器的实际参数和其形式参数是相同的类型，比如两者都是类型 T 或者都是类型 *T；
+// 2、接收器实参是类型 T，但接收器形参是类型 *T，编译器会隐式地为我们取变量的地址；
+// 3、接收器实参是类型 *T，形参是类型 T，编译器会隐式地为我们取到指针指向的实际变量。
+
 type Point struct{ X, Y float64 }
 
 // 参数 p 叫做方法的接收器（receiver），建议是可以使用其类型的第一个字母
@@ -36,6 +41,18 @@ func main() {
 		{1, 1},
 	}
 	fmt.Println(perim.Distance()) // 12
+
+	(&q).ScaleBy(0.5)
+	fmt.Println(q) // {2 3}
+	// 语法糖，编译器会隐式地帮我们用 &q 去调用 ScaleBy 这个方法。
+	// 只适用于“变量”，包括 struct 里的字段比如 p.X，以及 array 和 slice 内的元素比如 perim[0]。
+	q.ScaleBy(2)
+	fmt.Println(q) // {4 6}
+	// 不能通过一个无法取到地址的接收器来调用指针方法，比如临时变量的内存地址就无法获取得到
+	// Point{1, 2}.ScaleBy(2) // compile error: cannot call pointer method ScaleBy on Point
+
+	var i *IntList
+	fmt.Println(i == nil, i.Sum()) // true 0
 }
 
 // 可以给同一个包内的任意命名类型定义方法，只要这个命名类型的底层类型不是指针或者 interface。
@@ -50,4 +67,36 @@ func (path Path) Distance() float64 {
 		}
 	}
 	return sum
+}
+
+// 当调用一个函数时，会对其每一个参数值进行拷贝，如果一个函数需要更新一个变量，或者函数的其中一个参数实在太大，我们希望能够避免进行这种默认的拷贝，这种情况下我们就需要用到指针了。
+// 对应到我们这里用来更新接收器的对象的方法，当这个接受者变量本身比较大时，我们就可以用其指针而不是对象来声明方法
+func (p *Point) ScaleBy(factor float64) {
+	// 方法的名字是 (*Point).ScaleBy，这里的括号是必须的。
+
+	(*p).X *= factor
+	// p.X *= factor
+	// 语法糖，编译器在这里也会给我们隐式地插入 * 这个操作符
+	p.Y *= factor
+}
+
+// 一般会约定如果 Point 这个类有一个指针作为接收器的方法，那么所有 Point 的方法都必须有一个指针接收器，即使是那些并不需要这个指针接收器的函数。
+
+// 只有类型（Point）和指向他们的指针(*Point)，才可能是出现在接收器声明里的两种接收器。
+// 为了避免歧义，在声明方法时，如果一个类型名本身是一个指针的话，是不允许其出现在接收器中的。
+type P *int
+
+// func (P) f() {} // compile error: invalid receiver type
+
+// 注意：nil 也是一个合法的接收器类型，比如对于 map 或者 slice 来说，nil 是合法的零值。
+type IntList struct {
+	Value int
+	Tail  *IntList
+}
+
+func (list *IntList) Sum() int {
+	if list == nil {
+		return 0
+	}
+	return list.Value + list.Tail.Sum()
 }
